947190379c1cdeeb2f6813c1bee8985e343920d3
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2025-04-23 18:07:18 -0400

    Add files via upload

--- housing-build-model.py
+++ housing-build-model.py
@@ -1,5 +1,5 @@
 # DSC 333
-# Builds KNN regression model from the kc_house_data dataset
+# Builds KNN and RF regression models from the kc_house_data dataset
 # and pickles it to current directory
 
 # Only run once to generate the .pkl file.  Must be executed
@@ -9,8 +9,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor
 import pickle
-
-print('Building KNN model. This may take a while.')
 
 df = pd.read_csv('https://raw.githubusercontent.com/dsc333/housing-prediction/main/kc_house_data.csv')
 
@@ -25,7 +23,8 @@ y = df.loc[:, 'price']
 # Generate the train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=1)
 
-# define the model.  Neighborhood size = 15 (try changing the neighborhood size)
+### KNN ###
+# Define KNN model.  Neighborhood size = 15 (try changing the neighborhood size)
 knn = KNeighborsRegressor(n_neighbors = 15, weights = 'uniform')
 
 # "Train" the model
@@ -34,7 +33,25 @@ knn.fit(X_train, y_train)
 # Run the model to make predictions based on test data
 y_pred = knn.predict(X_test)
 
-print('R squared: ', knn.score(X_test, y_test))
+print('R squared (KNN): ', knn.score(X_test, y_test))
 
 # Save model to pickle file for use by API 
 pickle.dump(knn, open('housing-knn.pkl', 'wb'))
+
+
+### Random Forest ###
+# Create the model
+rf_regr = RandomForestRegressor(max_depth=6, random_state=0)
+
+# Train the model
+rf_regr.fit(X_train, y_train)
+
+# Make predictions
+y_pred = rf_regr.predict(X_test)
+
+# Evaluate
+print('R squared (RF): ', rf_regr.score(X_test, y_test))
+
+# Save model to pickle file for use by API 
+pickle.dump(knn, open('housing-rf.pkl', 'wb'))
+
